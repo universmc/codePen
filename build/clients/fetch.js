72c9c8c0811069b122eb7f30c952ace3afd0cf48
universmc <hubmaster74@gmail.com>
2025-01-26 15:31:10 +0100

    auto commit

--- build/clients/fetch.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:3000/clients')
-  .then(response => response.json())
-  .then(data => {
-    // Afficher les données dans la console
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Erreur:', error);
-  });
